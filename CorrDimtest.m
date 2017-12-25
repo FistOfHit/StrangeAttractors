6b6bfaf61c0f0b09d8fa515fddbc34ae2f241670
Hitesh Kumar <hitesh.kumar15@imperial.ac.uk>
2017-12-25 01:08:25 +0000

    Delete CorrDimtest.m

--- CorrDimtest.m
+++
@@ -1,9 +0,0 @@
-
-numPoints = linspace(3.1, 5, 15);
-corrDims = zeros(1, 15);
-
-for i = 1 : length(numPoints)
-    corrDims(i) = CorrelationDimension(-0.2, 1, -1.8, 0.3, 0.05, ((sqrt(5) - 1) / 2), floor(10^(numPoints(i))));
-end
-
-plot(numPoints, corrDims);
